0b5269c0a45ee9852169a27955f0b17eed11c3b1
Daan Debie <debie.daan@gmail.com>
2014-05-27 01:48:18 +0200

    Working 404

--- autoplaylistpoetry/app.py
+++ autoplaylistpoetry/app.py
@@ -1,6 +1,6 @@
 import logging.config
 
-from flask import Flask
+from flask import Flask, render_template
 
 from autoplaylistpoetry.web import web
 from autoplaylistpoetry.api import api
@@ -19,6 +19,7 @@ def create_app(blueprints=None):
     app = Flask(__name__)
     config_app(app)
     config_blueprints(app, blueprints)
+    config_error_pages(app)
     return app
 
 
@@ -31,3 +32,9 @@ def config_app(app):
 def config_blueprints(app, blueprints):
     for blueprint in blueprints:
         app.register_blueprint(blueprint)
+
+
+def config_error_pages(app):
+    @app.errorhandler(404)
+    def page_not_found(e):
+        return render_template('web/404.html'), 404
